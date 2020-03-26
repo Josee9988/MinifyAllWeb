23090c0a89ba695109f71b4e91e20237b3ae6f26
Josee9988 <jgracia9988@gmail.com>
2020-03-26 15:57:37 +0100

    "navbar component and routes prepared"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
 
-
-const routes: Routes = [];
+/**
+ * all routes of which the application is composed by.
+ */
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'}, // no route specified (go to home)
+  {path: 'home', component: AppComponent},
+  {path: '**', component: AppComponent} // fallback route (not found - 404)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
