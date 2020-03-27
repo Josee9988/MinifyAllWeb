c72d166b6c177a439b10a2337c8d4f8108c357b6
Josee9988 <jgracia9988@gmail.com>
2020-03-27 22:47:05 +0100

    "extra condition when no language was found"

--- src/app/shared/services/detect-language.service.ts
+++ src/app/shared/services/detect-language.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {LanguagesEnum} from '../enums/Languages.enum';
+import {SnackbarDisplayerService} from './snackbar-displayer.service';
+import {SnackbarTypeEnum} from '../enums/Snackbar-type.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,9 @@ import {LanguagesEnum} from '../enums/Languages.enum';
  * Class that detects with the given text if the code source is CSS/HTML/JSON
  */
 export class DetectLanguageService {
+
+  constructor(private snackbarDisplayerService: SnackbarDisplayerService) {
+  }
 
   /**
    * Summary: receives a code and guesses the programming language that it is written in.
@@ -22,7 +27,12 @@ export class DetectLanguageService {
     } else if (source.match(/{?(?<key>.*?):(?<val>["].*["]?|.*?)[,\W]\}/g) ||
       source.match(/(?:\s*\S+\s*{[^}]*})+/)) {
       return LanguagesEnum.JSON;
-    } else {
+      // tslint:disable-next-line:max-line-length
+    } else if (source.match(/^(?:[\s]+)?(?:const|let|var|)?(?:[a-z0-9.]+(?:\.prototype)?)?(?:\s)?(?:[a-z0-9-_]+\s?=)?\s?(?:[a-z0-9]+\s+\:\s+)?(?:function\s?)?(?:[a-z0-9_-]+)?\s?\(.*\)\s?(?:.+)?([=>]:)?\{(?:(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}(?:\s?\(.*\)\s?\)\s?)?)?(?:\;)?/gmi)) {
+      return LanguagesEnum.JAVASCRIPT;
+    } else { // not 100% secure what it is, so let's return JS (almost impossible that this condition will go on)
+      this.snackbarDisplayerService.openSnackBar(
+        'We are not fully sure about the language, please review it manually', SnackbarTypeEnum.informational);
       return LanguagesEnum.JAVASCRIPT;
     }
   }
