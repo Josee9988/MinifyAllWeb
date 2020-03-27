8a621f6e919df02bfafe1ed87b5156ee5dce9769
Josee9988 <jgracia9988@gmail.com>
2020-03-27 22:51:58 +0100

    "css and json multiple positives message"

--- src/app/shared/services/detect-language.service.ts
+++ src/app/shared/services/detect-language.service.ts
@@ -19,21 +19,39 @@ export class DetectLanguageService {
    * @param source the programming code to analyze.
    */
   detectLanguage(source: string): LanguagesEnum {
+    // HTML CODE
     if (source.match(/<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/) || source.match(/<(?:br|p)[^>{]*>|<\w+\s*>/gmi)) {
       return LanguagesEnum.HTML;
-    } else if (source.match(
-      /[\#\.\w\-\r\t]+(?=\s*\{)/)) {
+
+      // CSS AND JSON ARE POSITIVE (NOT SURE MESSAGE)
+    } else if (source.match(/[\#\.\w\-\r\t]+(?=\s*\{)/) && (source.match(/{?(?<key>.*?):(?<val>["].*["]?|.*?)[,\W]\}/g) ||
+      source.match(/(?:\s*\S+\s*{[^}]*})+/))) {
+      this.displayNotSureMessage();
       return LanguagesEnum.CSS;
+
+      // CSS CODE
+    } else if (source.match(/[\#\.\w\-\r\t]+(?=\s*\{)/)) {
+      return LanguagesEnum.CSS;
+
+      // JSON CODE
     } else if (source.match(/{?(?<key>.*?):(?<val>["].*["]?|.*?)[,\W]\}/g) ||
       source.match(/(?:\s*\S+\s*{[^}]*})+/)) {
       return LanguagesEnum.JSON;
+
+      // JAVASCRIPT
       // tslint:disable-next-line:max-line-length
     } else if (source.match(/^(?:[\s]+)?(?:const|let|var|)?(?:[a-z0-9.]+(?:\.prototype)?)?(?:\s)?(?:[a-z0-9-_]+\s?=)?\s?(?:[a-z0-9]+\s+\:\s+)?(?:function\s?)?(?:[a-z0-9_-]+)?\s?\(.*\)\s?(?:.+)?([=>]:)?\{(?:(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}(?:\s?\(.*\)\s?\)\s?)?)?(?:\;)?/gmi)) {
       return LanguagesEnum.JAVASCRIPT;
+
+      // ANY OF THE OTHERS MATCHED
     } else { // not 100% secure what it is, so let's return JS (almost impossible that this condition will go on)
-      this.snackbarDisplayerService.openSnackBar(
-        'We are not fully sure about the language, please review it manually', SnackbarTypeEnum.informational);
+      this.displayNotSureMessage();
       return LanguagesEnum.JAVASCRIPT;
     }
+  }
+
+  private displayNotSureMessage(): void {
+    this.snackbarDisplayerService.openSnackBar(
+      'We are not fully sure about the language, please review it manually', SnackbarTypeEnum.warning);
   }
 }
