f3b03a5d11be7f925762f53bfb54822d4ba7ee57
Josee9988 <jgracia9988@gmail.com>
2020-03-28 09:20:05 +0100

    "changelog shows as a page same as readme"

--- src/app/components/changelog/changelog.component.ts
+++ src/app/components/changelog/changelog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MarkdownParserService} from '../../shared/services/markdown-parser.service';
 
 @Component({
   selector: 'app-changelog',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./changelog.component.scss']
 })
 export class ChangelogComponent implements OnInit {
+  convertedText: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private markdownParserService: MarkdownParserService) {
   }
 
+  ngOnInit() {
+    this.markdownParserService.getChangelog().subscribe(Markdown => {
+      this.convertedText = this.markdownParserService.markdownToHtml(Markdown);
+    });
+  }
 }
