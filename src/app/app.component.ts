f970c649994847a6e8ec10720e7fe0145560a57e
Josee9988 <jgracia9988@gmail.com>
2020-04-17 11:02:49 +0200

    "support for github pages SPA application"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MinifyAll';
+
+  /**
+   * https://medium.com/swlh/how-to-host-your-angular-reactjs-vuejs-spa-on-github-pages-2d9ab102ac7b
+   * HACK TO ALLOW TO USE SPA IN GITHUB PAGES
+   * TODO: if purchasing a server remove this
+   */
+  constructor(private router: Router) {
+    const path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
 }
