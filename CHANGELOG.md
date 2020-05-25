d10450999a2e52c3c1158a9382d2452517d61a49
Josee9988 <jgracia9988@gmail.com>
2020-05-25 18:46:45 +0200

    "canonical tag as a service"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ---
 
+## [Unreleased]
+
+### Added
+
+* Canonical link on page refresh.
+
 ## [**1.3.2**] - 2020-05-23
 
 ### Added
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {CanonicalService} from "./shared/services/canonical.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   /**
    * https://medium.com/swlh/how-to-host-your-angular-reactjs-vuejs-spa-on-github-pages-2d9ab102ac7b
    * HACK TO ALLOW TO USE SPA IN GITHUB PAGES
    * TODO: if purchasing a server remove this
    */
-  constructor(private router: Router) {
+  constructor(private router: Router, private canonicalService: CanonicalService) {
     const path = localStorage.getItem('path');
     if (path) {
       localStorage.removeItem('path');
       this.router.navigate([path]);
     }
+  }
+
+  ngOnInit(): void {
+    this.canonicalService.setCanonicalURL();
+
   }
 }
