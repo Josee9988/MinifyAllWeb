d65df19a10ed17174196016ddd15a0ec4d122be5
Josee9988 <jgracia9988@gmail.com>
2020-05-27 10:07:55 +0200

    "meta tags set dinamically for each route using the new seoservice"

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MarkdownParserService} from '../../shared/services/markdown-parser.service';
 import {LocalFilesService} from '../../shared/services/local-files.service';
-import {Meta, Title} from "@angular/platform-browser";
+import {SeoService} from "../../shared/services/seo.service";
 
 @Component({
   selector: 'app-information',
@@ -14,15 +14,17 @@ export class InformationComponent implements OnInit {
   constructor(
     private markdownParserService: MarkdownParserService,
     private localFilesService: LocalFilesService,
-    private title: Title,
-    private meta: Meta) {
+    private seo: SeoService) {
   }
 
   ngOnInit() {
     this.localFilesService.getReadme().subscribe(Markdown => {
       this.convertedText = this.markdownParserService.markdownToHtml(Markdown);
     });
-    this.title.setTitle('MinifyAll changelog');
-    this.meta.updateTag({name: 'Changelog page of MinifyAll displaying the CHANGELOG.md file from the repository.'})
+    this.seo.generateTags({
+      title: 'MinifyAll information - minifyall.jgracia.es',
+      description: 'Informational page of MinifyAll displaying the README.md file from the Github repository.',
+      slug: 'info'
+    });
   }
 }
