caea04c4c5a13f24396c332b4c36a5efd5a39a1c
Josee9988 <jgracia9988@gmail.com>
2020-05-30 15:49:16 +0200

    "multiple e2e tests for the main page"

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,15 +1,15 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(path: string) {
+    return browser.get(path) as Promise<any>;
   }
 
-  getH1TitleText(){
-    return element(by.css('h1')).getText() as Promise<string>;
+  getElementTextByTagName(tag: string){
+    return element(by.tagName(tag)).getText() as Promise<string>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getElementAttributeByCss(css: string, attribute: string){
+    return element(by.css(css)).getAttribute(attribute) as Promise<string>;
   }
 }
--- e2e/src/home-minifier.e2e-spec.ts
+++ e2e/src/home-minifier.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {AppPage} from './app.po';
+import {browser, logging} from 'protractor';
 import {environment} from "../../src/environments/environment";
 
 describe('Home minifier \'/\' default route', () => {
@@ -7,11 +7,40 @@ describe('Home minifier \'/\' default route', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo('/');
+  });
+
+  it('should have the expected title', () => {
+    expect(browser.getTitle()).toEqual('MinifyAll - ' + environment.domainUrl);
+    expect(browser.getTitle()).toContain('minifyall.');
+  });
+
+  it('should have the expected canonical url tag', () => {
+    expect(page.getElementAttributeByCss("link[rel='canonical']", 'href')).toContain(environment.production ? environment.url : 'localhost');
   });
 
   it('should display h1 tag with the name and the domain url', () => {
-    page.navigateTo();
-    expect(page.getH1TitleText()).toEqual('MinifyAll - '+environment.domainUrl);
+    const h1Tag = page.getElementTextByTagName('app-root app-home-component h1');
+    expect(h1Tag).toEqual('MinifyAll - ' + environment.domainUrl);
+    expect(h1Tag).toContain('minifyall.');
+  });
+
+  it('should display h2 tag with a brief description', () => {
+    const h2Tag = page.getElementTextByTagName('app-root app-home-component h2');
+    expect(h2Tag).toContain('tool');
+    expect(h2Tag).toContain('MinifyAll');
+    expect(h2Tag).toContain('to make it lighter');
+    expect(h2Tag).toContain('minify your code');
+  });
+
+  it('should display main button with the expected text', () => {
+    const minifyMainButton = page.getElementTextByTagName('app-root app-home-component .mat-raised-button');
+    expect(minifyMainButton).toContain('Minify code');
+    expect(minifyMainButton).not.toContain('select');
+    expect(minifyMainButton).not.toContain('copy');
+    expect(minifyMainButton).not.toContain('Select');
+    expect(minifyMainButton).not.toContain('Copy');
+    expect(minifyMainButton).not.toContain('text');
   });
 
   afterEach(async () => {
